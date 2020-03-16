8c0c013319ae57be5100fd520576aa3b60c5f579
Mary Kanu <maryikanu@gmail.com>
2020-03-16 02:33:46 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,8 @@
+# Dont do this:
+puts (1+1)
+# Perform a constant expression evaluation
+3
+#Assign a constant 3 to bare-word variable triangle-sides
+'triangle-sides'
+print "triangle-sides"
+# Commenting out is an important hack for sharing and explaining to other session
